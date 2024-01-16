89fc076b1d3d84dbfc424fc18e8ddf236855f7d3
Juste_ozan <117074595+osahinGuardia@users.noreply.github.com>
2024-01-16 12:15:15 +0100

    add rev_rotate commands

---
+++ commands/rev_rotate.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rev_rotate.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/16 11:35:28 by marvin            #+#    #+#             */
+/*   Updated: 2024/01/16 11:35:28 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/push_swap.h"
+
+void	rev_rotate(t_stack **stack)
+{
+	t_stack	*last;
+	t_stack	*second_last;
+
+	if (!(*stack) || !((*stack)->next))
+		return ;
+	last = *stack;
+	second_last = NULL;
+	while (last->next)
+	{
+		second_last = last;
+		last = last->next;
+	}
+	last->next = *stack;
+	*stack = last;
+	second_last->next = NULL;
+}
+
+void	rra(t_stack **a)
+{
+	rev_rotate(a);
+	write(1, "rra\n", 4);
+}
+
+void	rrb(t_stack **b)
+{
+	rev_rotate(b);
+	write(1, "rrb\n", 4);
+}
+
+void	rrr(t_stack **a, t_stack **b)
+{
+	rev_rotate(a);
+	rev_rotate(b);
+	write(1, "rrr\n", 4);
+}
